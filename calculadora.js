45ec6f6ffd576ba75b64d453831d8d7281387cf0
Santiago <santiago.valle1245@gmail.com>
2026-01-20 15:11:28 -0600

    agrega ejercicio calculadora

---
+++ calculadora.js
@@ -0,0 +1,14 @@
+//alert("Hola mundo");
+
+function Mostrar() {
+
+    let numero1 = parseInt (document.getElementById("numero1").value);
+    let numero2 = parseInt (document.getElementById("numero2").value);
+
+    let resultado = 0
+    resultado = numero1 + numero2;
+
+    let objResultado = document.getElementById("resultado");
+    objResultado.innerHTML = resultado;
+
+}
