89aea3bae7cdfae92d975fa398875537d043f739
Santiago <santiago.valle1245@gmail.com>
2026-01-22 13:36:52 -0600

    cambios de calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,14 +1,37 @@
 //alert("Hola mundo");
 
-function Mostrar() {
+function Mostrar(e){
 
     let numero1 = parseInt (document.getElementById("numero1").value);
     let numero2 = parseInt (document.getElementById("numero2").value);
+    let resultado = 0;
+    let operacion = e.target.id;
 
-    let resultado = 0
-    resultado = numero1 + numero2;
+    if(operacion == "suma"){
+        resultado = numero1 + numero2;
+    }
+    else if (operacion == "resta"){
+        resultado = numero1 - numero2;
+    }
+    else if (operacion == "multiplica"){
+        resultado = numero1 * numero2;
+    }
+    else if (operacion == "divide"){
+        resultado = numero1 / numero2;
+    }
+    else{
+        alert("Funcion en proceso :)")
+    }
+
 
     let objResultado = document.getElementById("resultado");
     objResultado.innerHTML = resultado;
 
+    //alert(resultado);
+
 }
+
+let botones = document.getElementById("btns");
+botones.addEventListener("click", Mostrar);
+
+
